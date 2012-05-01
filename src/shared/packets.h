f550b7a47feeb501214c4baf49b13c7ba41c8fea
MOZGIII <mike-n@narod.ru>
2012-05-01 22:59:45 +0400

    Implemented server shutdown packet

--- src/shared/packets.h
+++ src/shared/packets.h
@@ -12,6 +12,7 @@ typedef struct packet_auth_request{
 	char passw[ENCRYPTED_PASSWORD_LENGTH];
 } packet_auth_request;
 
+
 /* PACKET S2C - Auth Response */
 
 #define PACKET_AUTH_RESPONSE 2
@@ -19,3 +20,10 @@ typedef struct packet_auth_response{
 	uint8_t response;
 } packet_auth_response;
 
+
+/* PACKET C2S - Server shutdown request (admin only) */
+
+#define PACKET_SERVER_SHUTDOWN 3
+typedef struct packet_server_shutdown{
+	// empty
+} packet_server_shutdown;
