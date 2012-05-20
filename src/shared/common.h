e070b60376faeae2a1d972be43a87fdb313ce706
MOZGIII <mike-n@narod.ru>
2012-05-20 17:16:43 +0400

    Reimplemented desk representation

--- src/shared/common.h
+++ src/shared/common.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+
+/* Common data */
+
 // content must be allocated and freed manually
 // length must be converted to network byte-order if transferring over
 typedef struct binary_data_t {
@@ -10,18 +13,31 @@ typedef struct binary_data_t {
 	char * content;
 } binary_data_t;
 
-typedef struct deck{
-	cell cells[64];
-} deck;
 
-typedef struct figure{
+/* Game data */
+
+// Figures
+#define FIGURE_NONE   0
+#define FIGURE_KING   1
+#define FIGURE_QUEEN  2
+#define FIGURE_ROOK   3
+#define FIGURE_KNIGHT 4
+#define FIGURE_BISHOP 5
+#define FIGURE_PAWNS  6
+
+// Game desk representation, ready to transfer over the network
+#pragma pack(push, 1)
+typedef struct figure {
 	uint8_t type;
 	uint8_t team;
 } figure;
 
-typedef struct cell {
-	uint8_t isempty;
-	figure f;
-} cell;
+typedef figure cell;
+
+typedef struct desk {
+	cell cells[64];
+} desk;
+#pragma pack(pop)
+
 
 #endif
